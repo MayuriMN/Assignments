22db5c2e7e90c57f718b3456564132a59d0c02f0
MayuriMN <111779308+MayuriMN@users.noreply.github.com>
2022-08-29 21:52:16 +0530

    Add files via upload

---
+++ Assignment5OOpsCal/ClassA.java
@@ -0,0 +1,33 @@
+package Assignment5OOpsCal;
+
+public class ClassA  extends CalcAbs{
+
+	@Override
+	void sum(int a, int b) {
+		
+		System.out.println("Addtion =" +(a+b));
+		
+	}
+
+	@Override
+	void sub(int a, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void mul(int a, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void div(int a, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+}
---
+++ Assignment5OOpsCal/ClassD.java
@@ -0,0 +1,11 @@
+package Assignment5OOpsCal;
+
+public class ClassD extends ClassC {
+
+	@Override
+	void div(int a, int b) {
+		System.out.println("Division =" +(a/b));
+		
+	}	
+
+}
